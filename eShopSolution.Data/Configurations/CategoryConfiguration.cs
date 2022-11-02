cd9d0c4a4cab4d9261af9ad7f4e61c658895bf74
tungdao1102 <daotung492@gmail.com>
2022-11-03 01:34:58 +0700

    change all

---
+++ eShopSolution.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //   throw new NotImplementedException();
+            {
+                builder.ToTable("Categories");
+                builder.HasKey(x => x.Id);
+                builder.Property(x => x.SortOrder).IsRequired();
+                builder.Property(x => x.IsShowOnHome).IsRequired();
+                builder.Property(x => x.Status).HasDefaultValue(Status.Active);
+                builder.Property(x => x.ParentId);
+            }
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
